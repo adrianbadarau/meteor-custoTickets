2766f9bf618c2756e1d49e05ed0347620a4e662d
adrianbadarau <adi.badarau@yahoo.com>
2015-10-18 16:01:32 +0300

    made the ticket create and index pages

---
+++ client/templates/my_tickets/my_tickets.js
@@ -0,0 +1,23 @@
+Template.my_tickets.events({
+	'submit #open_ticket_form': function (event) {
+		event.preventDefault();
+
+		var ticket = {
+			name: event.target.name_field.value,
+			email: event.target.email_field.value,
+			subject: event.target.subject_field.value,
+			department: event.target.department_select.value,
+			priority: event.target.priority_select.value,
+			message: event.target.message_t_area.value,
+			staus: "new",
+			customer: Meteor.userId(),
+			createdAt: new Date(),
+		}
+
+		var ticketId = Tickets.insert(ticket);
+
+		$('#open_ticket').modal('hide');
+
+		FlashMessages.sendSuccess("Your ticket "+ticketId+" has been succesfuly created")
+	}
+});
